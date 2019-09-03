ac815db97e60c7e4389f0b040aed9ca778b99543
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-03 15:19:50 -0500

    queue based requests

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -5,27 +5,60 @@ const download = require('download-file');
 const baseURL = 'https://bijao.uninorte.edu.co';
 const mainURL = '/AFS/ServiceDesk/Services/';
 
-let interval;
-let filesRequested = 0;
-let intervalStarted = false;
+let timeoutSteps = 1;
+let minTimeoutDuration = 1000;
+let timeoutStepLength = 5000;
+let maxTimeoutSteps = 100;
 
-let pending = [];
+let filesRequested = 0;
+
+let pendingFiles = [];
+let pendingDirectories = [];
 
 mainRequest = function(url, dir) {
-    directoryRequest(url,dir);
-
-    setTimeout(startInterval,120000);
+    addDirectoryRequest(url,dir);
+    nextDirectoryTimeout();
 };
 
-startInterval = function() {
-    intervalStarted = true;
-    interval = setInterval(doPending,60000);
+reduceTimeout = function() {
+  timeoutSteps = Math.max(timeoutSteps-1, 0);
+};
+
+increaseTimeout = function() {
+    timeoutSteps = Math.min(timeoutSteps+1, maxTimeoutSteps)
+};
+
+nextDirectoryTimeout = function() {
+    setTimeout(requestPendingDirectory,getTimeoutDuration());
+};
+
+nextFileTimeout = function() {
+    setTimeout(requestPendingFile,getTimeoutDuration());
+};
+
+getTimeoutDuration = function() {
+  return   Math.max((timeoutStepLength*timeoutSteps),minTimeoutDuration);
+};
+
+addDirectoryRequest = function(url,dir) {
+    pendingDirectories[pendingDirectories.length] = {
+        url,
+        dir
+    };
+};
+
+addFileRequest = function(url, options) {
+    pendingFiles[pendingFiles.length] = {
+        file : url,
+        options
+    };
 };
 
 directoryRequest = function(url,dir) {
-    console.log('Requesting directory');
+    console.log('Requesting directory '+url);
     request(url, (error, response, html) => {
         if (!error && response.statusCode === 200) {
+            reduceTimeout();
             const $ = cheerio.load(html);
             $('a').each((i, el) => {
                 const item = $(el).text();
@@ -34,53 +67,81 @@ directoryRequest = function(url,dir) {
                 if (link.endsWith('/')) {
                     console.log('Found directory: ' + item);
                     console.log('Located in url: ' + baseURL + link);
-                    directoryRequest(baseURL+link,dir+item+'/')
+                    addDirectoryRequest(baseURL+link,dir+item+'/');
                 } else {
                     filesRequested ++;
                     console.log('Found document: ' + item);
                     console.log('Located in url: ' + baseURL + link);
-                    fileRequest(baseURL+link,{directory : dir, filename : item});
+                    addFileRequest(baseURL+link,{directory : dir, filename : item});
                 }
             });
         } else {
-            console.log('Error fetching directory')
+            console.log('Error requesting directory, adding back to requests');
+            addDirectoryRequest(url,dir);
+            increaseTimeout();
         }
     });
 
 };
 
 fileRequest = function(file, options) {
-    console.log('Downloading file at url '+file);
+    console.log('Downloading file '+file);
     download(file, options,function(e){
         if (e) {
-            console.log('Error fetching file. Adding to pending');
-            pending[pending.length] = {
-                file, options
-            };
+            console.log('Error fetching file, adding back to requests');
+            addFileRequest(file,options);
+
+            increaseTimeout();
+        } else {
+            reduceTimeout();
         }
     });
 };
 
-doPending = function() {
-    console.log('Pending files: ' + pending.length);
-    if (pending.length > 0) {
-        const firstFile = pending.shift();
+requestPendingDirectory = function() {
+    if (pendingDirectories.length > 0) {
+        const firstDirectory = pendingDirectories.shift();
+        const currentDirectoryURL = firstDirectory.url;
+        const currentDirectoryDir = firstDirectory.dir;
+        directoryRequest(currentDirectoryURL,currentDirectoryDir);
+
+        nextDirectoryTimeout();
+    } else {
+        nextFileTimeout();
+    }
+};
+
+requestPendingFile = function() {
+    if (pendingFiles.length > 0) {
+        const firstFile = pendingFiles.shift();
         const currentFile = firstFile.file;
         const currentOptions = firstFile.options;
         fileRequest(currentFile,currentOptions);
+
+        nextFileTimeout();
+    } else {
+        console.log('Forcing recheck');
+
+        setTimeout(checkPending,timeoutStepLength*maxTimeoutSteps/2);
     }
-    if (filesRequested > 0 && pending.length === 0) {
-        clearInterval(interval);
-        console.log('Downloaded '+filesRequested+' files');
+};
+
+checkPending = function() {
+    if (pendingDirectories.length > 0) {
+        nextDirectoryTimeout();
+    } else {
+        if (pendingFiles.length > 0) {
+            nextFileTimeout();
+        } else {
+            console.log('Finished')
+        }
     }
 };
 
 
 
-
-/*
 mainRequest(baseURL + mainURL, './scraped-files/');
-*/
+
 /*
 Cheerio tutorial: [DONE]
 https://www.youtube.com/watch?v=LoziivfAAjE
